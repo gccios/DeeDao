68fca5cfc82259319c27201cbacb676ec2bea83b
郭春城 <gcc@lifesea.com>
2018-05-09 09:52:29 +0800

    Initial Commit

---
+++ DeeDao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeeDao
+//
+//  Created by 郭春城 on 2018/5/9.
+//  Copyright © 2018年 郭春城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeeDao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeeDao
+//
+//  Created by 郭春城 on 2018/5/9.
+//  Copyright © 2018年 郭春城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
